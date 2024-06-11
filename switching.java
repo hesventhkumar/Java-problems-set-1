29b4b99ab35da2d72ae0397053d6da790c5ae47f
hesventhkumar <141727454+hesventhkumar@users.noreply.github.com>
2024-06-11 12:27:15 +0530

    Create switching.java

---
+++ switching.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main{
+public static void main(String args[]){
+ int num= 49;
+        switch(num){
+            case '1' :
+                System.out.print("hello");
+                break;
+            case 2:
+                System.out.print("world");
+                break;
+            case 3:
+                System.out.print("hi");
+                break;
+            default:
+                System.out.print("no");
+        }
+ }
